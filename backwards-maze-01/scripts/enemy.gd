6be9dda1046161d664e1ef8f3afe4c9366eea766
fuckyouformakingmemakeaaccount <holcombjackson5@gmail.com>
2026-09-05 18:39:29 -0400

    added an an animated sprite for the enemy

--- backwards-maze-01/scripts/enemy.gd
+++ backwards-maze-01/scripts/enemy.gd
@@ -1,21 +1,31 @@
+# Script for enemy 
 extends CharacterBody3D
+var dbugPrints = false # Variable that prints to the Output tab for debugging
+
 
 var player = null
 
-const SPEED = 4.0
+var movementSpeed = 4.0 # Determines movement speed
 
+# Animations 
+@onready var animations: AnimatedSprite3D = $AnimatedSprite3D
+# Pathfinding
 @export var player_path : NodePath
+@onready var navAgentNode = $NavigationAgent3D
 
-@onready var navAgent = $NavigationAgent3D
 func _ready() -> void:
 	player = get_node(player_path)
 	
 func _physics_process(delta: float) -> void:
-	# MOVEMENT
-	velocity = Vector3.ZERO
-	navAgent.set_target_position(player.global_transform.origin)
-	if true:
-		print("enemy: navAgent", navAgent)
-	var nextNavPoint = navAgent.get_next_path_position()
-	velocity = (nextNavPoint - global_transform.origin).normalized() * SPEED
+	
+
+	# Pathfinding
+	#velocity = Vector3.ZERO
+	navAgentNode.set_target_position(player.global_transform.origin)
+	if dbugPrints:
+		print("enemy: navAgentNodeNode", navAgentNode)
+	var nextNavPoint = navAgentNode.get_next_path_position()
+	velocity = (nextNavPoint - global_transform.origin).normalized() * movementSpeed
+	#animations.animation("idle")
+	#look_at(Vector3(player.global_position.x, global_pop))
 	move_and_slide()
